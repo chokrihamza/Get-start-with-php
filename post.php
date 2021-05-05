97b9ec576af417932e71fbe4255f79f1521c7590
chokrihamza <chokrihamza1994@gmail.com>
2021-05-05 12:18:51 +0200

    adding post file

---
+++ post.php
@@ -0,0 +1,22 @@
+<?php
+echo $_POST['name'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+
+  <body>
+    <form action="post.php" method="post">
+      <input type='text' name="name" />
+      <input type="submit" />
+    </form>
+  </body>
+
+</html>
